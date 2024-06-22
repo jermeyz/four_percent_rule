a9d17d6452c9ef852552c64f550c8d30d4fd68a6
JZ <jeremy_zeidner@yahoo.com>
2024-06-22 09:12:33 -0400

    more formatting

--- four_percent_rule.py
+++ four_percent_rule.py
@@ -5,54 +5,56 @@ from vega_datasets import data
 import altair as alt
 
 @st.cache_data
-def build_data_set(principal,future_percent_return,periods,withdrawl_rate,rate_of_inflation):
+def build_data_set(principal, future_percent_return, periods, withdrawal_rate, rate_of_inflation):
     _init_amount = principal
     _periods = periods
     _future_percent_return = future_percent_return
-    _withdrawl_rate = withdrawl_rate
-    df = pd.DataFrame(index=range(_periods ), columns=[ 'principal','interest_earned','withdrawl_amount'])
-    df.at[0,'principal'] = _init_amount
-    df.at[0,'interest_earned'] = _init_amount * _future_percent_return
-    df.at[0,'withdrawl_amount'] = (df.at[0,'interest_earned'] +  _init_amount) * _withdrawl_rate
-    last_withdrawal_percent = _withdrawl_rate
+    _withdrawal_rate = withdrawal_rate
+    df = pd.DataFrame(index=range(_periods), columns=['principal', 'interest_earned', 'withdrawal_amount'])
+    df.at[0, 'principal'] = _init_amount
+    df.at[0, 'interest_earned'] = _init_amount * _future_percent_return
+    df.at[0, 'withdrawal_amount'] = (df.at[0, 'interest_earned'] + _init_amount) * _withdrawal_rate
+    last_withdrawal_percent = _withdrawal_rate
     for period in range(1, _periods + 1):
-        previous = df.at[period-1,'principal']
-        interest = previous  * ( _future_percent_return)
-        df.at[period,'interest_earned'] = interest
+        previous = df.at[period - 1, 'principal']
+        interest = previous * _future_percent_return
+        df.at[period, 'interest_earned'] = interest
         new_balance = interest + previous
-        df.at[period,'withdrawl_amount'] =  (new_balance * last_withdrawal_percent)
+        df.at[period, 'withdrawal_amount'] = new_balance * last_withdrawal_percent
         last_withdrawal_percent = last_withdrawal_percent * (1 + rate_of_inflation)
-        df.at[period,'principal'] = new_balance - (df.at[period,'withdrawl_amount'])
+        df.at[period, 'principal'] = new_balance - df.at[period, 'withdrawal_amount']
 
     df.index.name = "index"
     df.reset_index(inplace=True)
     return df
-def build_data_set_actual_data(principal,withdrawl_rate,real_data):
-    init_amount = principal
+
+def build_data_set_actual_data(principal, withdrawal_rate, real_data):
+    _principal = principal
     steps = 30
-    withdrawl_percent = withdrawl_rate
-    df = pd.DataFrame(index=range(steps ), columns=[ 'principal','interest_earned','withdrawl_amount','yearly_returns','inflation_rate'])
-    df.at[0,'principal'] = init_amount
-    df.at[0,'interest_earned'] = init_amount * (real_data.at[0,"sp_returns"] /100)
-    df.at[0,'withdrawl_amount'] = (df.at[0,'interest_earned'] +  init_amount) * withdrawl_percent
-    last_withdrawal_percent = withdrawl_percent
-    for period in range(1, steps ):
-        previous = df.at[period-1,'principal']
-        interest = previous  * (real_data.at[period,"sp_returns"] /100)
-        df.at[period,'interest_earned'] = interest
-        df.at[period,"yearly_returns"] = real_data.at[period,"sp_returns"] 
-        df.at[period,"inflation_rate"] = real_data.at[period,"inflation_rate"] 
+    _withdrawal_rate = withdrawal_rate
+    df = pd.DataFrame(index=range(steps), columns=['principal', 'interest_earned', 'withdrawal_amount', 'yearly_returns', 'inflation_rate'])
+    df.at[0, 'principal'] = _principal
+    df.at[0, 'interest_earned'] = _principal * (real_data.at[0, "sp_returns"] / 100)
+    df.at[0, 'withdrawal_amount'] = (df.at[0, 'interest_earned'] + _principal) * _withdrawal_rate
+    last_withdrawal_percent = _withdrawal_rate
+    for period in range(1, steps):
+        previous = df.at[period - 1, 'principal']
+        interest = previous * (real_data.at[period, "sp_returns"] / 100)
+        df.at[period, 'interest_earned'] = interest
+        df.at[period, "yearly_returns"] = real_data.at[period, "sp_returns"]
+        df.at[period, "inflation_rate"] = real_data.at[period, "inflation_rate"]
         new_balance = interest + previous
-        df.at[period,'withdrawl_amount'] =  (new_balance * last_withdrawal_percent)
-        last_withdrawal_percent = last_withdrawal_percent * (1 + (real_data.at[period,"inflation_rate"] / 100))
-        df.at[period,'principal'] = new_balance - (df.at[period,'withdrawl_amount'])
+        df.at[period, 'withdrawal_amount'] = new_balance * last_withdrawal_percent
+        last_withdrawal_percent = last_withdrawal_percent * (1 + (real_data.at[period, "inflation_rate"] / 100))
+        df.at[period, 'principal'] = new_balance - df.at[period, 'withdrawal_amount']
 
     df.index.name = "index"
     df.reset_index(inplace=True)
     return df
+
 def get_chart(data):
     hover = alt.selection_point(
-        fields=["index","principal"],
+        fields=["index", "principal"],
         nearest=True,
         on="mouseover",
         empty="none",
@@ -62,8 +64,8 @@ def get_chart(data):
         alt.Chart(data, title="Growth of initial principal over time factoring in withdrawals")
         .mark_line()
         .encode(
-            x=alt.X("index",axis=alt.Axis(title="Years")),
-            y=alt.Y("principal",axis=alt.Axis(title="Total Amount Remaining")) 
+            x=alt.X("index", axis=alt.Axis(title="Years")),
+            y=alt.Y("principal", axis=alt.Axis(title="Total Amount Remaining"))
         )
     )
     tooltips = (
@@ -75,7 +77,7 @@ def get_chart(data):
             opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
             tooltip=[
                 alt.Tooltip("index", title="Year"),
-                alt.Tooltip("principal",format='$,.2f', title="Price (USD)"),
+                alt.Tooltip("principal", format='$,.2f', title="Total Amount Remaining"),
             ],
         )
         .add_params(hover)
@@ -83,17 +85,16 @@ def get_chart(data):
 
     points = lines.transform_filter(hover).mark_circle(size=65)
 
-    return (lines + points +tooltips)#.interactive()
-
+    return lines + points + tooltips  # .interactive()
 
 '''
 # 4% Withdrawal Rule Calculator
 
 This is a demonstration of the 4% rule.
 
-This rule says you can withdraw no more than 4% per year in retirement for the first year and adjust the withdrawl amount for inflation every year after.  
+This rule says you can withdraw no more than 4% per year in retirement for the first year and adjust the withdrawal amount for inflation every year after.  
 
-This should allow your money to last approx 30 years.
+This should allow your money to last approximately 30 years.
 
 **As an example:**
 
@@ -103,55 +104,56 @@ This should allow your money to last approx 30 years.
 >
 > _Assuming zero rate of return_
 >
-> ***First year you can withdrawal = (.04 * 100,000) =  $4000***
+> ***First year you can withdraw = (.04 * 100,000) =  $4000***
 >
-> ***Second year you can withdrawal = ((.04 * 1.03) * 96,000) = $3955.20***
+> ***Second year you can withdraw = ((.04 * 1.03) * 96,000) = $3955.20***
 '''
 
 with st.sidebar:
-    principal = st.number_input("**Starting amount**",value=1000000,key="principal",step=100)
-    percent_increase = st.number_input("**Average percent gain for future**",value=8.0,key="percent_increase",step=1.0)
-    years = st.number_input("**Years to live**",value=30,key="years",step=1)
-    percent_withdrawl = st.number_input("**Percent Withdrawl**",value=4.0,key="percent_withdrawl",step=1.0)
-    inflation_rate =  st.number_input("**Inflation Rate**",value=2.0,key="inflation_rate",step=0.5)
+    principal = st.number_input("**Starting amount**", value=1000000, key="principal", step=100)
+    percent_increase = st.number_input("**Average percent gain for future**", value=8.0, key="percent_increase", step=1.0)
+    years = st.number_input("**Years to live**", value=30, key="years", step=1)
+    percent_withdrawal = st.number_input("**Percent Withdrawal**", value=4.0, key="percent_withdrawal", step=1.0)
+    inflation_rate = st.number_input("**Inflation Rate**", value=2.0, key="inflation_rate", step=0.5)
 
-
-
-source = build_data_set(principal,percent_increase/100,years,percent_withdrawl/100,inflation_rate/100)
+source = build_data_set(principal, percent_increase / 100, years, percent_withdrawal / 100, inflation_rate / 100)
 real_returns = {
-"Year": ["1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"],
-"sp_returns": [7.06, -1.54, 34.11, 20.26, 31.01, 26.67, 19.53, -10.14, -13.04, -23.37, 26.38, 8.99, 3.00, 13.62, 3.53, -38.49, 23.45, 12.78, 0.00, 13.41, 29.60, 11.39, -0.73, 9.54, 19.42, -6.24, 28.88, 16.26, 26.89, -19.44, 24.23],
-"inflation_rate": [2.70, 2.70, 2.50, 3.30, 1.70, 1.60, 2.70, 3.40, 1.60, 2.40, 1.90, 3.30, 3.40, 2.50, 4.10, 0.10, 2.70, 1.50, 3.00, 1.70, 1.50, 0.80, 0.70, 2.10, 2.10, 1.90, 2.30, 1.40, 7.00, 6.50, 3.40]
+    "Year": ["1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"],
+    "sp_returns": [7.06, -1.54, 34.11, 20.26, 31.01, 26.67, 19.53, -10.14, -13.04, -23.37, 26.38, 8.99, 3.00, 13.62, 3.53, -38.49, 23.45, 12.78, 0.00, 13.41, 29.60, 11.39, -0.73, 9.54, 19.42, -6.24, 28.88, 16.26, 26.89, -19.44, 24.23],
+    "inflation_rate": [2.70, 2.70, 2.50, 3.30, 1.70, 1.60, 2.70, 3.40, 1.60, 2.40, 1.90, 3.30, 3.40, 2.50, 4.10, 0.10, 2.70, 1.50, 3.00, 1.70, 1.50, 0.80, 0.70, 2.10, 2.10, 1.90, 2.30, 1.40, 7.00, 6.50, 3.40]
 }
 rdf = pd.DataFrame(real_returns)
 
-with st.container(border=True):
+avg_returns = rdf['sp_returns'].mean()
+avg_inflation = rdf['inflation_rate'].mean()
 
+with st.container():
     st.write(f"**Total interest earned:** ${source['interest_earned'].sum():,.2f}")
-    st.write(f"**Total amount withdrawn:** ${source['withdrawl_amount'].sum():,.2f}")
-
-
-   
+    st.write(f"**Total amount withdrawn:** ${source['withdrawal_amount'].sum():,.2f}")
     st.altair_chart(
-        (get_chart(source) ),
+        get_chart(source),
         use_container_width=True
-)
-styled_dataset = source.style.format({'principal': '${:,.2f}','interest_earned': '${:,.2f}','withdrawl_amount': '${:,.2f}'})
+    )
+
+renamed_source = source.rename(columns={
+    'principal': 'Principal Amount',
+    'interest_earned': 'Interest Earned',
+    'withdrawal_amount': 'Withdrawal Amount'
+}).drop(columns=['index'])
+styled_dataset = renamed_source.style.format({'Principal Amount': '${:,.2f}', 'Interest Earned': '${:,.2f}', 'Withdrawal Amount': '${:,.2f}'})
 
 st.table(styled_dataset)
 
-real_data = build_data_set_actual_data(principal,percent_withdrawl/100,rdf)
-styled_dataset = real_data.style.format({'principal': '${:,.2f}','interest_earned': '${:,.2f}','withdrawl_amount': '${:,.2f}'})
+real_data = build_data_set_actual_data(principal, percent_withdrawal / 100, rdf)
+styled_dataset = real_data.style.format({'principal': '${:,.2f}', 'interest_earned': '${:,.2f}', 'withdrawal_amount': '${:,.2f}'})
 
 with st.container(border=True):
-    st.write(f"**Total interest earned REAL:** ${real_data['interest_earned'].sum():,.2f}")
-    st.write(f"**Total amount withdrawn REAL:** ${real_data['withdrawl_amount'].sum():,.2f}")
-    
+    st.write(f"**Total interest earned against real data:** ${real_data['interest_earned'].sum():,.2f}")
+    st.write(f"**Total amount withdrawn  against real data:** ${real_data['withdrawal_amount'].sum():,.2f}")
+    st.write(f"**Average Returns:** {avg_returns/100:,.2%}")
+    st.write(f"**Average Inflation:** {avg_inflation/100:,.2%}")
     st.altair_chart(
-        (get_chart(real_data) ),
-        use_container_width=True)
-    styled_dataset
-
-# Create DataFrame
-
-#https://www.macrotrends.net/2526/sp-500-historical-annual-returns
+        get_chart(real_data),
+        use_container_width=True
+    )
+    st.table(styled_dataset)
